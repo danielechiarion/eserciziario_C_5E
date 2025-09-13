bc1cc66b562201ef56a3a53e0f5cb984f7c0017a
il deniel <150442406+danielechiarion@users.noreply.github.com>
2025-09-13 09:01:08 +0200

    Continue first exercise to be finished

--- ripasso/Chiarion_5E_Es1A_ripasso.c
+++ ripasso/Chiarion_5E_Es1A_ripasso.c
@@ -9,7 +9,7 @@ int generateNumber(int min, int max){
 }
 
 /* function which fills an array using numbers */
-void generateArray(int array[], int size, zint minNumber, int maxNumber){
+void generateArray(int array[], int size, int minNumber, int maxNumber){
 	/* populate the array with the numbers */
 	for(int i=0; i<size;i++)
 		array[i] = generateNumber(min, max)	
@@ -33,23 +33,73 @@ int findPosition(int array[], int size, int number){
 	return -1;
 }
 
-int main(){
+/* Function to display a menu with all choices */
+int displayMenu(char options[][], int size){
+	int choice; // define variable
+	do{
+		printf("Scegli tra le seguenti opzioni: \n");
+		for(int i=0;i<size;i++){
+			printf("%d) %s", (i+1), options[i]);
+		}
+		scanf("%d", &choice); // input of the choice
+		
+		if(choice <= 0 || choice > size + 1)
+			printf("Numero non valido\n");
+	}while(choice<=0 || choice > size + 1);
+	
+	return choice;
+}
+
+int main(int argc, char *argv[]){
 	/* define variables */
 	int choice;
-	int sizeArray, numberSearch;
+	int sizeArray, maxArrayNumer, minArrayNumber;
+	int numberSearch;
+	
+	/* define constant values */
+	const int MINDSIZEARRAY = 3;
 	
 	/* define the choices for the menu */
 	char options = {
-		{"Popola l'array"}, 
+		{"Visualizza numeri"}, 
 		{"Visualizza l'array invertito"},
 		{"Visualizza somma e media"},
 		{"Visualizza numeri pari"},
 		{"Visualizza numeri dispari"},
 		{"Ricerca numero"},
-    {"Elimina numero"},
-    
+  {"Elimina numero"},
+  {"Alterna posizioni del vettore"},
+		{"Ordina il vettore"},
+		{"Esci"}
 	};
-    
-    return 0;
-
+	
+	/* populate the array by the dimension, 
+	maximum and minimum given */
+	do{
+		printf("Inserisci la dimensione dell'array: ");
+		scanf("%d", &sizeArray);
+		
+		if(sizeArray<MINSIZEARRAY)
+			printf("La dimensione minima dell'array deve essere %d", MINSIZEARRAY);
+	}while(sizeArray<MINSIZEARRAY);
+	
+	printf("Inserisci il numero minimo dell'array: ");
+	scanf("%d", &minArrayNumber);
+	
+	do{
+		printf("Inserisci il numero massimo dell'array: ");
+		scanf("%d", &maxArrayNumber);
+		
+		if(maxArrayNumber<=minArrayNumber)
+			printf("Il numero massimo dell'array deve essere strettamente maggiore del minimo");
+	}while(maxArrayNumber<=minArrayNumber); // check if the maximum number of the array is higher than the minimum
+	
+	/* declare the array and use the function to instantiate it */
+	int array[sizeArray];
+	generateArray(array, sizeArray, minArrayNumber, maxArrayNumber);
+	
+	do{
+		
+	}while(choice != sizeof(options)/sizeof(options[0])); // continue till the end button is pressed
+ return 0;
 }
